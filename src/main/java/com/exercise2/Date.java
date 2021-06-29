a5ff80f9514bc6f2a825912983453987f98b9f88
Phisa Bya <phisa.bya@gmail.com>
2021-06-29 10:05:25 -0400

    Date

--- src/main/java/com/exercise2/Date.java
+++ src/main/java/com/exercise2/Date.java
@@ -10,5 +10,32 @@ public class Date {
 		this.day = day;
 		this.year = year;
 	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
 	
+	public void displayDate() {
+		System.out.println(this.month + "/" + this.day + "/" + this.year);
+	}
 }
---
+++ src/main/java/com/exercise2/DateTest.java
@@ -0,0 +1,14 @@
+package com.exercise2;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date d1 = new Date(6,29,2021);
+		d1.displayDate();
+		d1.setDay(30);
+		d1.displayDate();
+
+	}
+
+}
